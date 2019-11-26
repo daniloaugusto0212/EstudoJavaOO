3b188211b4b72437880d7e508489fb476c30e60c
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2019-11-26 20:27:54 -0300

    Exemplo While

---
+++ Exemplos/src/exemplos/Exemplo03While.java
@@ -0,0 +1,16 @@
+
+package exemplos;
+
+
+public class Exemplo03While {
+    public static void main(String[] args) {
+        int numero = 0;
+        while(numero != 5){
+            System.out.print(numero);
+            System.out.println(" é menor que 5");
+            numero++;
+        }    
+    
+    }
+}
+    
