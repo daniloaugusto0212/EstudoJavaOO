fefa9404e2f2aead156a8f3a78ececa4094f0f74
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2019-11-26 22:01:48 -0200

    Objeto Caneta

---
+++ Exemplos2/src/exemplos2/Exemplos2.java
@@ -0,0 +1,15 @@
+
+package exemplos2;
+
+public class Exemplos2 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.destampar();        
+        c1.status();
+        c1.rabiscar();
+
+    }
+}
