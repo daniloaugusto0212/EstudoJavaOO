df8d2e0d0c5e2ded5e083a13e457bc85dea3299d
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2019-11-26 22:08:28 -0300

    Teste

--- Exemplos2/src/exemplos2/Exemplos2.java
+++ Exemplos2/src/exemplos2/Exemplos2.java
@@ -7,9 +7,16 @@ public class Exemplos2 {
         Caneta c1 = new Caneta();
         c1.cor = "Azul";
         c1.ponta = 0.5f;
-        c1.destampar();        
+        c1.tampar();        
         c1.status();
         c1.rabiscar();
+        
+        Caneta c2 = new Caneta();
+        c2.modelo = "Bic";
+        c2.cor = "Preta";
+        c2.destampar();
+        c2.status();
+        c2.rabiscar();
 
     }
 }
