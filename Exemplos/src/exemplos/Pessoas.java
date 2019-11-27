89037b9d9abbf882c83e543651eade2390269b10
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2019-11-26 21:03:54 -0300

    Objeto Caneta

---
+++ Exemplos/src/exemplos/Pessoas.java
@@ -0,0 +1,27 @@
+
+package exemplos;
+
+
+public class Pessoas {
+    
+    //Seus atributos
+    public String nome, gênero;
+    public int idade;
+    public float peso, altura;
+
+    //Seu métodos
+    public void andar(){
+        //ações do método andar
+
+    }
+    public void falar(){
+        //ações do método falar
+
+    }
+    public void comer(){
+        //açõe do método comer
+        
+    }
+    
+    
+}
---
+++ JavaOO/Iniciando/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,12 @@
+
+package tiposprimitivos;
+
+public class TiposPrimitivos {
+
+    public static void main(final String[] args) {
+        final String nome = "Danilo";
+        final float nota = 8.5f;
+        System.out.printf("A nota de %s é %.2f", nome, nota);
+    }
+    
+}
