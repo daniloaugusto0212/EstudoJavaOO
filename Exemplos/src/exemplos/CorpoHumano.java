365f618db97e20d82863e0b66d0980f08a70ec18
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2019-11-28 21:06:11 -0300

    Exemplo de associação

---
+++ Exemplos/src/exemplos/CorpoHumano.java
@@ -0,0 +1,34 @@
+/*
+EXEMPLO DE ASSOCIAÇÃO
+
+public class CorpoHumano {
+    //declarando variável do tipo Membro
+    public Membro membro1 = new Membro;
+    public Membro membro2 = new Membro;
+    public Membro membro3 = new Membro;
+    public Membro membro4 = new Membro;
+
+    //Métodos da classe CorpoHumano
+
+    public void pegar(){
+        System.out.println("Pegando!");
+
+    }
+    public void andar() {
+        System.out.println("Andando!");
+    }
+
+    public class Membro{
+        //Declarando variável
+        int forcaDoMembro;
+        
+        //Método
+        public void fazerForca() {
+            System.out.println("Fazendo força com os membros!");
+        }
+
+    }
+
+
+}
+*/
